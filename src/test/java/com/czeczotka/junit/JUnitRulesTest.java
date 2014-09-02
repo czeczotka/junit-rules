c34760c9d2097b3def4d6b42babe68ca203e9403
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2014-09-02 22:05:07 +0100

    Add TestName rule

--- src/test/java/com/czeczotka/junit/JUnitRulesTest.java
+++ src/test/java/com/czeczotka/junit/JUnitRulesTest.java
@@ -1,4 +1,20 @@
 package com.czeczotka.junit;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+/**
+ * @author Jakub Czeczotka
+ * @since 02/09/14
+ */
 public class JUnitRulesTest {
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Test
+    public void printTestMethodName() {
+        System.out.println("Test method name: " + name.getMethodName());
+    }
 }
