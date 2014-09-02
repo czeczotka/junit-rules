cf5d1fe6ace571b76a5da4df23a9c12b70b56a4e
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2014-09-02 23:30:05 +0100

    Add Timeout rule

--- src/test/java/com/czeczotka/junit/JUnitRulesTest.java
+++ src/test/java/com/czeczotka/junit/JUnitRulesTest.java
@@ -3,6 +3,7 @@ package com.czeczotka.junit;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.rules.Timeout;
 
 /**
  * @author Jakub Czeczotka
@@ -13,8 +14,16 @@ public class JUnitRulesTest {
     @Rule
     public TestName name = new TestName();
 
+    @Rule
+    public Timeout timeout = new Timeout(20);
+
     @Test
     public void printTestMethodName() {
         System.out.println("Test method name: " + name.getMethodName());
+    }
+
+    @Test
+    public void slowTestAskingForTrouble() throws InterruptedException {
+        Thread.sleep(1000 * 60);
     }
 }
